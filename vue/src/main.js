39a7d30d912596fffd2a62b61b7911828c2b01b2
LiYu <2248868140@qq.com>
2022-01-08 17:41:50 +0800

    完成404页面，使其错误是重定向 和 引入js-cookies

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,9 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import EventBus from './bus/eventBus'
 import './assets/less/normalize.less'
 
 const $bus = new EventBus()
-Vue.prototype.$bus = $bus
+
 
 createApp(App).use(store).use(router).mount('#app')
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,17 +4,14 @@ import Home from '../views/Home.vue'
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'login',
+    component: () => import('../views/Login')
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: "/404",
+    component: () => import("../views/404"),
+  },
+  { path: "/:pathMatch(.*)*", redirect: "/404" }, //错误页面重定象
 ]
 
 const router = createRouter({
